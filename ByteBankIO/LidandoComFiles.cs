c97f7d0c3b28cc90327853a5d5253fae5dfa64da
hugo de araujo <hugo.saraujo@tutanota.com>
2024-07-03 09:17:43 -0300

    Aprimorando a leitura de arquivos da aplicação.

---
+++ ByteBankIO/LidandoComFiles.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+partial class Program
+{
+    public void LidandoComFiles()
+    {
+        var enderecoArquivo = "contas.txt";
+
+        using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+            var buffer = new byte[1024];
+    
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+
+                Console.WriteLine($"\nbytes lidos, {numeroDeBytesLidos}");
+                EscreverBuffer(buffer, numeroDeBytesLidos);    
+            }
+            fluxoArquivo.Close();
+        }
+    }
+    
+    public static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+    }
+}
+
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.ReadLine();
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoArquivo);
+
+             
+            // var texto = leitor.ReadToEnd();
+            // var lerByte = leitor.Read();
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
+        Console.Read(); 
+    }
+}
